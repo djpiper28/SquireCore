4513dda8a1a7a3bb137f9c68462781a4c92c1b27
Danny Piper <djpiper28@gmail.com>
2022-07-25 16:28:04 +0100

    feat: fix Unix builds (python3 not python)

--- ffi_build.sh
+++ ffi_build.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-python bindgen.py
+python3 bindgen.py
 cbindgen --config cbindgen.toml --crate squire_lib --output squire_core.h -v
 echo "Exported to ./squire_core.h"
 cargo build --features ffi --package squire_lib
